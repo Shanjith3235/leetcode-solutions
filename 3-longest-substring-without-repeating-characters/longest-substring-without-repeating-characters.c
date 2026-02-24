392a4a0d9338ffad1f0c6de672fb360e6014b55a
Shanjith SS <23ecc21@karpagamtech.ac.in>
2026-02-24 19:44:11 +0530

    Time: 14 ms (15.32%) | Memory: 9.1 MB (84.73%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -0,0 +1,19 @@
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+int lengthOfLongestSubstring(char* s) {
+    int n = strlen(s);
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        bool vis[128] = {false};
+        for (int j = i; j < n; j++) {
+            if (vis[s[j]]) {
+                break;
+            } else {
+                res = max(res, j - i + 1);
+                vis[s[j]] = true;
+            }
+        }
+    }
+    return res;
+}
